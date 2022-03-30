432c34843f6a829e28f9e52e809e638c7f22b47b
Hugo Lemerle <hlml200294@gmail.com>
2022-03-30 21:46:35 +0200

    CRUD User

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,14 +1,89 @@
 const db = require('../database').getDB();
+const validator = require('validator');
+const bcrypt = require('bcrypt');
 
-module.exports.signUp = (req, res, next) => {
-    console.log(req.body);
-    const sql = `INSERT INTO users (pseudo, email, password) VALUES ('${req.body.pseudo}', '${req.body.email}', '${req.body.password}')`;
+module.exports.signUp = async (req, res, next) => {
+    try {
+        const {pseudo, email, password} = req.body;
+        const salt = await bcrypt.genSalt(10);
+        const cryptedPassword = await bcrypt.hash(password, salt);
+        const sql = `INSERT INTO users (pseudo, email, password) VALUES ('${pseudo}', '${email}', '${cryptedPassword}')`;
+        db.query(sql, (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(400).json({err});
+            }
+            else if (!validator.isEmail(email)) {
+                const sql = `DELETE FROM users WHERE pseudo='${pseudo}' AND email='${email}' AND password='${cryptedPassword}'`;
+                db.query(sql, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log(result);
+                    }
+                });
+                console.log('No GG !');
+                res.status(400).json({message: 'No GG !'});
+            }
+            else {
+                res.status(200).json(result);
+            }
+        });
+    }
+    catch (err) {
+        res.status(400).json({err});
+    }
+};
+
+module.exports.getAllUsers = (req, res, next) => {
+    const sql = `SELECT * FROM users`;
     db.query(sql, (err, result) => {
         if (err) {
-            console.log(err);
-            res.status(404).json({err});
-            throw err;
+            res.status(400).json({err});
         }
-        res.status(200).json(result);
+        else {
+            res.status(200).json(result);
+        }
     });
-};
+}
+
+module.exports.getUser = (req, res, next) => {
+    const id = req.params.id;
+    const sql = `SELECT * FROM users WHERE id='${id}'`;
+    db.query(sql, (err, result) => {
+        if (err) {
+            res.status(400).json({err});
+        }
+        else {
+            res.status(200).json(result);
+        }
+    });
+}
+
+module.exports.updateUser = (req, res, next) => {
+    const pseudo = req.body.pseudo;
+    const id = req.params.id;
+    const sql = `UPDATE users SET pseudo='${pseudo}' WHERE id='${id}'`;
+    db.query(sql, (err, result) => {
+        if (err) {
+            res.status(400).json({err});
+        }
+        else {
+            res.status(200).json(result);
+        }
+    });
+}
+
+module.exports.deleteUser = (req, res, next) => {
+    const id = req.params.id;
+    const sql = `DELETE FROM users WHERE id='${id}'`;
+    db.query(sql, (err, result) => {
+        if (err) {
+            res.status(400).json({err});
+        }
+        else {
+            res.status(200).json(result);
+        }
+    });
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const userCtrl = require('../controllers/user.controller');
 
 router.post('/register', userCtrl.signUp);
+router.get('/', userCtrl.getAllUsers);
+router.get('/:id', userCtrl.getUser);
+router.put('/:id', userCtrl.updateUser);
+router.delete('/:id', userCtrl.deleteUser);
 
 module.exports = router;
