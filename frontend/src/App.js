b5a4997fb277568e64cde407eefc6b3f142c1b9e
Hugo Lemerle <hlml200294@gmail.com>
2022-04-17 15:02:14 +0200

    Context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Routes from './components/Routes';
+import { UidContext } from './components/AppContext';
+import axios from 'axios';
 
 const App = () => {
+	const [uid, setUid] = useState('');
+
+	useEffect(() => {
+		const fetchToken = async () => {
+			await axios.get(`${process.env.REACT_APP_API_URL}api/jwtid`, {withCredentials: true})
+				.then((res) => {
+					console.log(res);
+					setUid(res.data);
+				})
+				.catch((err) => console.log('No token'));
+		};
+		fetchToken();
+	}, []);
+
 	return (
-		<div>
+		<UidContext.Provider value={uid}>
 			<Routes />
-		</div>
+		</UidContext.Provider>
 	);
 };
 
